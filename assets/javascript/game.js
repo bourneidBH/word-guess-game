75e05bd5a0429f189fe6a01a91e97447cee466a5
Jason Bourne <m.bourne@wi.rr.com>
2019-04-19 12:36:39 -0500

    fixed repeated letter problem, fixed game over message display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //global variables
 
-var wordArray = ["HERMIONE", "HOGWARTS", "NIFFLER", "VOLDEMORT", "POLYJUICE", "WAND", "DOBBY", "NAGINI", "DUMBLEDORE", "HORCRUX", "POTION", "BUCKBEAK"];
+var wordArray = ["HERMIONE", "HOGWARTS", "NIFFLER", "VOLDEMORT", "POLYJUICE", "WAND", "DOBBY", "NAGINI", "DUMBLEDORE", "HORCRUX", "POTION", "BUCKBEAK", "WEASLEY", "HARRY", "HAGRID", "MALFOY", "DRAGON", "WIZARD"];
 // empty variable to hold current word choice from the array
 var word = "";
 // Empty variable to hold the actual letters in the word
@@ -9,6 +9,8 @@ var wordLetters = [];
 var numBlanks = 0;
 // Variable to record user guess
 userGuess = "";
+// Variable hide/show game over buttons
+var divGameOver = "";
 
 // Empty array to store the answer as it displays for the user
 var answerDisplay = [];
@@ -42,6 +44,7 @@ function newGame () {
     wrongGuessesLeft = 6;
     wrongLetters = [];
     answerDisplay = [];
+    correctLetters = [];
 
     //Add the correct number of blanks to the answerDisplay that corresponds with the length of the currentWord
 
@@ -55,14 +58,13 @@ function newGame () {
         document.getElementById("blanks").innerHTML = e;
     }
     displayWordBlanks();
-
-
-
    
 }
 
-//start the game on any key pressed
-document.onkeyup = function () {
+
+
+//press button to start game
+document.getElementById("startBtn").addEventListener("click", newGame); {
     newGame();
 
     // function to record user key choice. Change case to all caps.
@@ -73,11 +75,18 @@ document.onkeyup = function () {
         // check if user key choice is included in word. 
 
        if (wordLetters.indexOf(userGuess) != -1) {
-            correctLetters.push(userGuess);
 
             //for loop to step through each value in wordLetters array 
             //check each index for match to userGuess
             //for each match push userGuess to correctLetters 
+            for (var w = 0; w < wordLetters.length; w++) {
+                if (wordLetters[w] === userGuess) {
+                    correctLetters.push(userGuess);
+                    console.log("correct letter is at index " + wordLetters[w]);
+
+                }
+            }
+            
             console.log("correct letters are: " + correctLetters);
 
             //If key choice included in word display letter in appropriate spot. 
@@ -94,22 +103,31 @@ document.onkeyup = function () {
         //check for win. 
         if (correctLetters.length === wordLetters.length) {
             wins ++;
-            document.getElementById("winMessage").innerHTML = "You win!";
-            //Show game reset buttons
-            function gameOver() {
-                var element = document.getElementById("gameReset");
-                element.classList.toggle("resetShow");
+            document.getElementById("winMessage").innerHTML = "You win!" + "<br>";
+            document.getElementById("winMessage").classList.toggle("resetShow");
+
+            //Show game reset button
+           function gameOver() {
+                divGameOver = document.getElementById("gameReset");
+                divGameOver.className =+ " " + "resetShow";
+                //clear wrong letter list
+                wrongLetters = [];
+
               }
               gameOver();
         }
         //check for loss
         if (wrongGuessesLeft === 0) {
             losses ++;
-            document.getElementById("lossMessage").innerHTML = "Game over. You Lost!" + "<br>" + "The correct word is " + word;
-            //Show game reset buttons
+            document.getElementById("lossMessage").innerHTML = "Game over. You Lost!" + "<br>" + "The correct word is " + word + "<br>";
+            document.getElementById("lossMessage").classList.add("resetShow");
+
+            //Show game reset button
             function gameOver() {
-                var element = document.getElementById("gameReset");
-                element.classList.toggle("resetShow");
+                divGameOver = document.getElementById("gameReset");
+                divGameOver.className += " " + "resetShow";
+                //clear wrong letter list
+                wrongLetters = [];
               }
               gameOver();
 
@@ -127,11 +145,50 @@ document.onkeyup = function () {
     }
 
 
-    // Display end of game message
+// function to start game & load first word. Display blanks for each letter in word.
+function resetGame () {
+
+    //Computer selects a word from the array
+    word = wordArray[Math.floor(Math.random() * wordArray.length)];
+        console.log("The current word chosen is: " + word); // Testing via Console.Log
+
+    //Grab the current word and break it apart into each individual letter
+    wordLetters = word.split("");
+        console.log("The current word's letters are: " + wordLetters); // Testing via Console.Log
+
+    //Grab the current word and get the number of letters in it
+    numBlanks = wordLetters.length;
+        console.log("The number of letters in the current word is: " + numBlanks); // Testing via Console.Log
+
+    //Reset game variables needed to be cleared before each new game starts
+    wrongGuessesLeft = 6;
+    wrongLetters = [];
+    answerDisplay = [];
+    correctLetters = [];
+
+    //hide previous win/loss message
+    document.getElementById("winMessage").innerHTML = "";
+    document.getElementById("lossMessage").innerHTML = "";
+
+    //Add the correct number of blanks to the answerDisplay that corresponds with the length of the currentWord
+
+        function displayWordBlanks() {
+        var e = "";   
+            
+        for (var y=0; y<wordLetters.length; y++)
+        {
+            e += "_ ";
+        }
+        document.getElementById("blanks").innerHTML = e;
+    }
+    displayWordBlanks();
+   
+}
 
 
     // function to load next word.
-
+    document.getElementById("playAgain").addEventListener("click", resetGame);
+    resetGame();
 
 
 }
